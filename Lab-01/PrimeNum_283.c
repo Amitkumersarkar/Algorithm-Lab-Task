97aff1d920f9ec8cae6c93ab6643e298b765132f
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-06 21:09:24 +0600

    added new task file

--- Lab-01/PrimeNum_283.c
+++ Lab-01/PrimeNum_283.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 int main()
 {
-    
+    int num = 19;
+    int count = 0;
+
+    if (num <= 1)
+    {
+        printf("The number is not prime: %d\n", num);
+    }
+    else
+    {
+        for (int j = 1; j <= num; j++)
+        {
+            if (num % j == 0)
+            {
+                count++;
+            }
+        }
+
+        if (count > 2)
+        {
+            printf("The number is not prime: %d\n", num);
+        }
+        else
+        {
+            printf("The number is prime: %d\n", num);
+        }
+    }
 
     return 0;
 }
